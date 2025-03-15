8a2dfbf65661587cd310f976f70f41c9695cd92a
chrisiogwaan <chris322322@gmail.com>
2025-03-16 01:55:36 +1100

    +feat: add Home component with welcome message

---
+++ portfolio-cg/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import AnimatedComponent from "../components/AnimatedComponent";
+
+export default function Home() {
+    return (
+        <div className="p-2">
+            <h1 className="text-7xl font-bold">Welcome to My Portfolio!</h1>
+            {/* <AnimatedComponent /> */}
+            <br/>
+            <p>&gt; chrisio@laptop:/mnt/e$ htop</p>
+        </div>
+    );
+}
