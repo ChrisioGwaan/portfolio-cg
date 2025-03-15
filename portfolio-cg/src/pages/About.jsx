af4d4ab7208ddafe6b95ec70829f9ab96733aef9
chrisiogwaan <chris322322@gmail.com>
2025-03-16 01:55:38 +1100

    +feat: add About component with personal introduction

---
+++ portfolio-cg/src/pages/About.jsx
@@ -0,0 +1,8 @@
+export default function About() {
+    return (
+        <div className="p-8">
+            <h1 className="text-3xl font-bold">About Me</h1>
+            <p>I am a software engineer.</p>
+        </div>
+    );
+}
