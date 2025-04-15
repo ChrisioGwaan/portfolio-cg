da0a7cddce3842c8aeec1febef6e9e408a54074f
Weixi (Chrisio) Guan <chris322322@gmail.com>
2025-04-15 20:15:40 +1000

    +refactor: cast request to any for accessing geo property in middleware

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 export function middleware(request: NextRequest) {
-  const country = request.geo?.country || 'AU';
+  const country = (request as any).geo?.country || 'AU';
   const host = request.headers.get('host') || '';
 
   const isFromChina = country === 'CN';
