6f5cdda8cd744238e8466833664797e605eb4fed
Weixi (Chrisio) Guan <chris322322@gmail.com>
2025-04-16 17:41:12 +1000

    +fix: correct spelling in footer copyright notice

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="mb-10 px-4 text-center text-gray-500">
       <small className="mb-2 block text-xs">
-        &copy; 2025 Weixi (Chrisio) Gwaan. All rights reserved.
+        &copy; 2025 Weixi (Chrisio) Guan. All rights reserved.
       </small>
     </footer>
   );
