a3496c9458c05b6935acbb302cc532335b11f8c5
Weixi (Chrisio) Guan <chris322322@gmail.com>
2025-05-01 15:18:07 +0800

    +update: integrate Speed Insights for performance monitoring in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import ThemeContextProvider from '@/context/theme-context';
 import { Toaster } from 'react-hot-toast';
 import { Analytics } from '@vercel/analytics/next';
 import LanguageSwitch from '@/components/language-switch';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -39,6 +40,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           </ActiveSectionContextProvider>
         </ThemeContextProvider>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
