9317478b29a52433c9c11c853d3da207141ef4e4
Weixi (Chrisio) Guan <chris322322@gmail.com>
2025-05-02 20:07:04 +0800

    +remove: comment out middleware function and related logic in middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,20 +1,20 @@
-import { NextRequest, NextResponse } from 'next/server';
+// import { NextRequest, NextResponse } from 'next/server';
 
-export function middleware(request: NextRequest) {
-  const country = (request as any).geo?.country || 'AU';
-  const host = request.headers.get('host') || '';
+// export function middleware(request: NextRequest) {
+//   const country = (request as any).geo?.country || 'AU';
+//   const host = request.headers.get('host') || '';
 
-  const isFromChina = country === 'CN';
-  const isCom = host.includes('chrisiogwaan.com') && !host.includes('.cn');
-  const isComCn = host.includes('chrisiogwaan.com.cn');
+//   const isFromChina = country === 'CN';
+//   const isCom = host.includes('chrisiogwaan.com') && !host.includes('.cn');
+//   const isComCn = host.includes('chrisiogwaan.com.cn');
 
-  if (isFromChina && isCom) {
-    return NextResponse.redirect('https://www.chrisiogwaan.com.cn');
-  }
+//   if (isFromChina && isCom) {
+//     return NextResponse.redirect('https://www.chrisiogwaan.com.cn');
+//   }
 
-  if (!isFromChina && isComCn) {
-    return NextResponse.redirect('https://www.chrisiogwaan.com');
-  }
+//   if (!isFromChina && isComCn) {
+//     return NextResponse.redirect('https://www.chrisiogwaan.com');
+//   }
 
-  return NextResponse.next();
-}
+//   return NextResponse.next();
+// }
