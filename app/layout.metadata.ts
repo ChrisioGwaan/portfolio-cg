fc0616396c52dba390b3f7edd91a844c966ba3e0
Chrisio Gwaan <weixi.guan@student.unimelb.edu.au>
2026-04-28 22:55:48 +1000

    +refactor: deep clean

--- app/layout.metadata.ts
+++
@@ -1,4 +0,0 @@
-export const metadata = {
-  title: 'Weixi Guan (Chrisio Gwaan, 关炜熙, 關煒熙) — Official Site',
-  description: 'Chrisio is a full-stack software developer with 1 year of experience.',
-};
