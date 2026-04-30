ebf153104035288073d89a174effbc1dc1d997e3
Chrisio Gwaan <weixi.guan@student.unimelb.edu.au>
2026-04-30 23:33:49 +1000

    +refactor: improve footer layout by enhancing responsiveness and truncating long text

--- components/footer.tsx
+++ components/footer.tsx
@@ -9,34 +9,34 @@ export default function Footer() {
   const { t } = useTranslation();
 
   return (
-    <footer className="py-8 px-4 mt-12 bg-gray-50 dark:bg-gray-900 border-t border-gray-200 dark:border-gray-800">
+    <footer className="mt-12 bg-gray-50 px-4 py-8 pb-24 dark:bg-gray-900 border-t border-gray-200 dark:border-gray-800 sm:pb-8">
       <div className="max-w-5xl mx-auto flex flex-col items-center gap-3">
-        <div className="flex gap-3 items-center">
-          <span className="flex items-center gap-1 hover:text-[#8cfa9e] transition">
-            <a href="mailto:chris322322@gmail.com" title="Email">
-              <LuMail className="inline-block" /> chris322322@gmail.com
+        <div className="grid w-full max-w-md grid-cols-1 gap-2 text-sm sm:max-w-none sm:grid-cols-2 lg:flex lg:items-center lg:justify-center lg:gap-3">
+          <span className="flex min-w-0 items-center justify-center gap-1 hover:text-[#8cfa9e] transition">
+            <a className="truncate" href="mailto:chris322322@gmail.com" title="Email">
+              <LuMail className="inline-block shrink-0" /> chris322322@gmail.com
             </a>
           </span>
 
           <span className="hidden sm:inline mx-2 text-gray-300 dark:text-gray-600">|</span>
 
-          <span className="flex items-center gap-1">
+          <span className="flex min-w-0 items-center justify-center gap-1">
             <TbBrandWechat className="inline-block" /> WeChat:{' '}
-            <span className="font-mono">chrisiogwaan</span>
+            <span className="font-mono truncate">chrisiogwaan</span>
           </span>
 
           <span className="hidden sm:inline mx-2 text-gray-300 dark:text-gray-600">|</span>
 
-          <span className="flex items-center gap-1">
+          <span className="flex min-w-0 items-center justify-center gap-1">
             <LuPhone className="inline-block" /> AU:{' '}
-            <span className="font-mono">+61&nbsp;411&nbsp;146&nbsp;832</span>
+            <span className="font-mono whitespace-nowrap">+61&nbsp;411&nbsp;146&nbsp;832</span>
           </span>
 
           <span className="hidden sm:inline mx-2 text-gray-300 dark:text-gray-600">|</span>
 
-          <span className="flex items-center gap-1">
+          <span className="flex min-w-0 items-center justify-center gap-1">
             <LuPhone className="inline-block" /> CN:{' '}
-            <span className="font-mono">+86&nbsp;150&nbsp;1186&nbsp;7647</span>
+            <span className="font-mono whitespace-nowrap">+86&nbsp;150&nbsp;1186&nbsp;7647</span>
           </span>
         </div>
 
@@ -62,7 +62,7 @@ export default function Footer() {
           </a>
         </div>
 
-        <small className="mt-2 text-xs text-gray-500">
+        <small className="mt-2 text-center text-xs text-gray-500">
           &copy; {new Date().getFullYear()} {t('footerCopyright')}
         </small>
 
